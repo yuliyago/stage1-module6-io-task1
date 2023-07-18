e00a24bb650249374cc1472b525a73f76ae40a74
yuliyago <yuuliya02@proton.me>
2023-07-18 17:59:23 +0300

    File parsing issues were solved.

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -5,26 +5,33 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
 
 
 public class FileReader {
+    static Logger logger = Logger.getLogger(FileReader.class.getName());
     public String getDataFromFileIntoString(File file) {
-        String rawDataFromFile = "";
+        String rawDataFromFile="";
+        StringBuilder bld = new StringBuilder();
         try (FileInputStream fileInputStream = new FileInputStream(file)) {
             int flag;
             while ((flag = fileInputStream.read()) != -1) {
-                rawDataFromFile += (char) flag;
+                bld.append ((char) flag);
             }
+            rawDataFromFile= bld.toString();
         } catch (FileNotFoundException e) {
-            System.out.println("Required file hasn't been found.");
+            logger.info("Required file hasn't been found.");
+//            System.out.println("Required file hasn't been found.");
         } catch (IOException e) {
-            System.out.println("Input/output exception has been thrown.");
+            logger.info("Input/output exception has been thrown.");
+//            System.out.println("Input/output exception has been thrown.");
         }
         return rawDataFromFile;
     }
 
-    public HashMap<String,String> transformStringIntoMap(String rawDataFromFile) {
-        HashMap<String,String> profileData=new HashMap<>();
+    public Map<String, String> transformStringIntoMap(String rawDataFromFile) {
+        HashMap<String, String> profileData = new HashMap<>();
         String[] lines = rawDataFromFile.split("\\r?\\n|\\r");
         String[] keyValue;
         for (String line : lines) {
@@ -36,7 +43,7 @@ public class FileReader {
 
     public Profile getDataFromFile(File file) {
         String rawDataFromFile = getDataFromFileIntoString(file);
-        HashMap<String,String> profileData = transformStringIntoMap(rawDataFromFile);
-        return new Profile(profileData.get("Name"),Integer.parseInt(profileData.get("Age")),profileData.get("Email"),Long.parseLong(profileData.get("Phone")));
+        Map<String, String> profileData = transformStringIntoMap(rawDataFromFile);
+        return new Profile(profileData.get("Name"), Integer.parseInt(profileData.get("Age")), profileData.get("Email"), Long.parseLong(profileData.get("Phone")));
     }
 }
